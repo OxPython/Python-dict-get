9e8e8c096de6987c09ff82d3609ff6b91a4783f4
Emerson <viejoemer@gmail.com>
2014-07-02 09:10:16 -0500

    #HowTo get a specific value from a dict in python?

---
+++ dict_get.py
@@ -0,0 +1,19 @@
+'''
+Created on Jul 1, 2014
+
+@author: viejoemer
+'''
+############################################################
+#HowTo get a specific value from a dict in python?
+
+#Creating a dict with data
+
+d = {
+     "red":100,
+     "yellow":200,
+     "blue" : 300
+     }
+
+print(d.get("red"))
+
+print(d)
